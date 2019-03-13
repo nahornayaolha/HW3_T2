469ed05c07dc2ce4a08491356c1e44a4c3d85fe8
Olha Nahorna <olha_nahorna@epam.com>
2019-03-13 23:44:49 +0200

    HW3_T2

--- HW3_Task2/Program.cs
+++ HW3_Task2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HW3_Task2
 {
@@ -10,6 +6,20 @@ namespace HW3_Task2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please add users to list or if you want to stop enter'stop':");
+            Users list = new Users();
+            list.addToList();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("OK, it is users created by you:");
+            list.listOfUsers();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("List in desc sorted order: ");
+            list.sortedListOfUsers();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Your short list is:");
+            list.RemoveRange();
+            Console.ReadKey();
+
         }
     }
 }
---
+++ HW3_Task2/Users.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW3_Task2
+{
+    class Users
+    {
+        bool added = true;
+        string newuser;
+
+
+        List<string> users = new List<string>() { };
+        public void addToList()
+        {
+            while (added)
+            {
+                newuser = Console.ReadLine();
+
+                if (newuser == "stop")
+                {
+                    break;
+                }
+
+                users.Add(newuser);
+            }
+        }
+
+        public void listOfUsers()
+        {
+            foreach (string newuser in users)
+                Console.Write($"{newuser} ");
+            
+        }
+        public void sortedListOfUsers()
+        {
+            users.Sort();
+            users.Reverse();
+            foreach (var newuser in users)
+                Console.Write($"{newuser} ");
+        }
+        public void RemoveRange()
+        {
+            int n = users.Count - 2;
+            users.RemoveRange(1, n);
+            foreach (var newuser in users)
+                Console.Write($"{newuser} ");
+        }
+    }
+}
